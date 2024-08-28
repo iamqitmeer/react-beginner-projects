4d1a73b8d15a7ca708df0b3f9bf4c1485f95c698
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-08-28 18:30:55 +0500

    push code

---
+++ beg_authenticationWithFirebase_app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './Screens/Login'
+import Register from './Screens/Register'
+
+function App() {
+
+  return (
+    <>
+      <Register />
+    {/* <Login /> */}
+    </>
+  )
+}
+
+export default App
