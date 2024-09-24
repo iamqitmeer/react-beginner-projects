ec49560866247951290c770d21a9c1939361bfa1
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-09-24 09:43:27 +0500

    push code

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,6 +1,87 @@
+import { DatePicker, Input, Select, SelectItem } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function App() {
-return (
-<></>   
+  let [expenseIncome, setExpenseIncome] = useState("");
+  let [expenseCategory, setExpenseCategory] = useState("");
+  let [expenseDate, setExpenseDate] = useState();
+
+  let [expenseList, setExpenseList] = useState([]);
+
+  function handleAddExpenseBtn() {
+    if (!expenseIncome || !expenseCategory || !expenseDate) {
+      alert("Plz Fill All Fields");
+    }
+
+    let expense = {
+      income: expenseIncome,
+      category: expenseCategory,
+      date: expenseDate,
+    };
+
+    setExpenseList([...expenseList, expense]);
+
+    expenseIncome("");
+    setExpenseCategory("Food");
+    setExpenseDate("");
+  }
+
+  return (
+    <div>
+      <div>
+        <Input
+          value={expenseIncome}
+          onChange={(e) => {
+            setExpenseIncome(e.target.value)            
+          }}
+          type="number"
+          label="Amount"
+        />
+
+        <DatePicker
+          value={expenseDate}
+          onChange={(e) => console.log(e.target.value)}
+          label="Birth date"
+        />
+        <Select
+          label="Favorite Animal"
+          placeholder="Select an animal"
+          value={expenseCategory}
+          onChange={(e) => setExpenseCategory(e.target.value)}
+        >
+          {["Food", "Entertainment", "Transport", "Utilities", "Other"].map(
+            (category) => (
+              <SelectItem>{category}</SelectItem>
+            )
+          )}
+        </Select>
+        {/* <select
+          value={expenseCategory}
+          onChange={(e) => setExpenseCategory(e.target.value)}
+          name=""
+          id=""
+        >
+          <option value="Food">Food</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Transport">Transport</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Other">Other</option>
+        </select> */}
+        <button onClick={handleAddExpenseBtn}>Add Expense</button>
+      </div>
+      <div>
+        {expenseList.length &&
+          expenseList.map((list, index) => {
+            return (
+              <div key={index}>
+                <h1>
+                  {list.income} {list.date}
+                </h1>
+                <p>{list.category}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
