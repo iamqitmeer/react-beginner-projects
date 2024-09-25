916029e028325e55e6db84e9c5f17b01aaa72563
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-09-26 00:30:59 +0500

    Half Done

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -13,9 +13,9 @@ export default function App() {
       <Header />
       <Hero />
       <About />
-      <Education />
       <TechStacks />
       <ProjectsSection />
+      <Education />
       <FuturesPlan />
       <Contact />
     </>
