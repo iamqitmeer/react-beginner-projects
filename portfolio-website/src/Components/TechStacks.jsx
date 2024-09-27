6f5b14952c576aec73abbefc4d59a3b1f89e91eb
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-09-27 11:36:00 +0500

    push code

--- portfolio-website/src/Components/TechStacks.jsx
+++ portfolio-website/src/Components/TechStacks.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Button, Image, Tooltip } from "@nextui-org/react";
 
 function TechStacks() {
   return (
-    <div>TechStacks</div>
-  )
+    <div className="w-full h-[700px] flex items-center flex-col justify-center border-b">
+      <h1 className="tracking-tighter text-4xl font-bold mb-12 mt-[140px]">
+        Tech Stacks.
+      </h1>
+
+      <div className="flex items-center flex-wrap justify-center w-[1100px] gap-16">
+        <Tooltip showArrow={true} color="primary" content="HTML">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 p-8 rounded-2xl cursor-pointer transition-all">
+            <i className="ri-html5-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="CSS">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-css3-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="Tailwind">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-tailwind-css-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="JavaScript">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-javascript-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="React.js">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-reactjs-line text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="Next.js">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-nextjs-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="Node.js">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-nodejs-line text-8xl"></i>
+          </div>
+        </Tooltip>
+        <Tooltip showArrow={true} color="primary" content="MongoDB">
+          <div className="hover:bg-blue-50 border-1 hover:border-blue-500 transition-all  p-8 rounded-2xl cursor-pointer">
+            <i className="ri-database-2-fill text-8xl"></i>
+          </div>
+        </Tooltip>
+      </div>
+    </div>
+  );
 }
 
-export default TechStacks
+export default TechStacks;
---
+++ todo-list-category-wise/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+function App() {
+  let [todoValue, setTodoValue] = useState("");
+  let [priorityValue, setPriorityValue] = useState("");
+  let [todoList, setTodoList] = useState([]);
+  let [categoryList, setCategoryList] = useState([]);
+  let [categoryValue, setCategoryValue] = useState("");
+
+  function handleClick() {
+    let createObj = {
+      title: todoValue,
+      priorityLevel: priorityValue,
+      category: categoryValue,
+    };
+
+    console.log(createObj);
+
+    let cloneArr = [...todoList];
+    cloneArr.push(createObj);
+    setTodoList(cloneArr);
+
+    setTodoValue("");
+  }
+
+  let handleDeleteBtn = (index) => {
+    let cloneArr = [...todoList];
+    cloneArr.slice(index, 1);
+    setTodoList(cloneArr);
+  };
+  let handleEditBtn = (index) => {
+    console.log(index);
+  };
+
+  let handleAddCategoryBtn = () => {
+    let cloneArr = [...categoryList];
+    cloneArr.push(categoryValue);
+    setCategoryList(cloneArr);
+
+    setCategoryValue("");
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          value={todoValue}
+          onChange={(e) => setTodoValue(e.target.value)}
+          type="text"
+          placeholder="Enter Todo Item Here."
+        />
+        <span>Priority Level: </span>
+        <select
+          onChange={(e) => setPriorityValue(e.target.value)}
+          value={priorityValue}
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <span>Category: </span>
+        <select
+          onChange={(e) => setCategoryValue(e.target.value)}
+          value={categoryValue}
+        >
+          {categoryList.map((ctgory) => {
+            return <option value={ctgory}>{ctgory}</option>;
+          })}
+        </select>
+        <button onClick={handleClick}>Add</button>
+        <ul>
+          {todoList.map((item, index) => {
+            return (
+              <li key={index}>
+                {item.title}
+                <span>{item.priorityLevel}</span>
+                <span>{item.category}</span>
+                <button onClick={() => handleDeleteBtn(index)}>Delete</button>
+                <button onClick={() => handleEditBtn(index)}>Edit</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div>
+        <input
+          value={categoryValue}
+          onChange={(e) => setCategoryValue(e.target.value)}
+          type="text"
+          placeholder="Enter Category"
+        />
+        <button onClick={handleAddCategoryBtn}>Add Category</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
