999374a3e821dedd9ee349e4dfaeecfeb3433083
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-10-01 12:26:41 +0500

    PUSH CODE

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
-return (
-<></>   
+  let [markdown, setMarkdown] = useState(() => {
+    let getMark = localStorage.getItem("markdown");
+
+    if (getMark) {
+      return getMark;
+    } else {
+      return "# Markdown Previewer";
+    }
+  });
+
+
+  localStorage.setItem("markdown", markdown);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Markdown Previewer</h1>
+
+      <div className="main">
+        <textarea
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+          className="right"
+          placeholder="Enter Markdown Here"
+        ></textarea>{" "}
+        <ReactMarkdown className={`left`}>{markdown}</ReactMarkdown>
+      </div>
+    </div>
   );
 }
---
+++ markdown-previewer/src/components/Right.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Right() {
+  return (
+   
+  )
+}
+
+export default Right
