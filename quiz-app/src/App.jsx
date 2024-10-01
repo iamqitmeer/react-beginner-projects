770a8a9b55b3804df8eff3705744a6cc02e3d385
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-10-01 21:00:24 +0500

    push code

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import quiz from "./quiz";
 
 export default function App() {
-return (
-<></>   
+  let [quizes, setQuizes] = useState(quiz);
+  let [currentQuestion, setCurrentQuestion] = useState(1);
+  let [result, setResult] = useState(0);
+
+  console.log(quizes);
+  console.log(quizes[currentQuestion - 1].incorrect_answers);
+
+  return (
+    <>
+      <h1>{quizes[currentQuestion - 1].question}</h1>
+      {quizes[currentQuestion - 1].incorrect_answers.map((answer) => {
+        return <button key={answer}>{answer}</button>;
+      })}
+      <button
+        onClick={() => {
+          setCurrentQuestion(currentQuestion + 1);
+        }}
+      >
+        Next
+      </button>
+    </>
   );
 }
---
+++ quiz-app/src/quiz.js
@@ -0,0 +1,122 @@
+let quiz = [
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Who is known as the 'God of Cricket'?",
+    correct_answer_index: 0,
+    incorrect_answers: [
+      "Sachin Tendulkar",
+      "Brian Lara",
+      "Ricky Ponting",
+      "Jacques Kallis",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Which country won the first ICC Cricket World Cup in 1975?",
+    correct_answer_index: 1,
+    incorrect_answers: ["India", "West Indies", "Australia", "England"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question:
+      "What is the maximum number of overs a bowler can bowl in a T20 match?",
+    correct_answer_index: 2,
+    incorrect_answers: ["2", "3", "4", "5"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question:
+      "Who holds the record for the highest individual score in a One Day International (ODI) match?",
+    correct_answer_index: 3,
+    incorrect_answers: [
+      "Rohit Sharma",
+      "Martin Guptill",
+      "Chris Gayle",
+      "Rohit Sharma",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Which cricketer is known for the 'helicopter shot'?",
+    correct_answer_index: 0,
+    incorrect_answers: [
+      "MS Dhoni",
+      "AB de Villiers",
+      "Virat Kohli",
+      "Kane Williamson",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Which country is known as the 'Proteas' in cricket?",
+    correct_answer_index: 1,
+    incorrect_answers: [
+      "Australia",
+      "South Africa",
+      "New Zealand",
+      "Sri Lanka",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question:
+      "Who was the captain of the Indian cricket team during the 1983 World Cup?",
+    correct_answer_index: 2,
+    incorrect_answers: [
+      "Sunil Gavaskar",
+      "Ravi Shastri",
+      "Kapil Dev",
+      "Mohinder Amarnath",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Which cricketer has the nickname 'The Wall'?",
+    correct_answer_index: 3,
+    incorrect_answers: [
+      "Rahul Dravid",
+      "Sourav Ganguly",
+      "VVS Laxman",
+      "Rahul Dravid",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Which country won the ICC T20 World Cup in 2021?",
+    correct_answer_index: 0,
+    incorrect_answers: ["Australia", "India", "England", "New Zealand"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Sports: Cricket",
+    question: "Who is the highest wicket-taker in Test cricket?",
+    correct_answer_index: 1,
+    incorrect_answers: [
+      "Shane Warne",
+      "Muttiah Muralitharan",
+      "Anil Kumble",
+      "James Anderson",
+    ],
+  },
+];
+
+export default quiz;
